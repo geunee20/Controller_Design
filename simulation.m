3a5d343ebfad8a7426cd7c6193e84db3c151de96
geunee20 <geunee20@gmail.com>
2024-05-21 11:11:02 -0400

    .

--- simulation.m
+++
@@ -1,7 +0,0 @@
-%% Day 1
-figure(1)
-plot(t, q1, t, q2, t, q3)
-title('Time vs. Angular Position')
-legend('Link 1', 'Link 2', 'Link 3', 'Location', 'best')
-xlabel('Time [s]')
-ylabel('Angular Displacement [rads]')
