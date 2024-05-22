7910c897a719b58f1bb41db6a5d0846504b49d10
geunee20 <geunee20@gmail.com>
2024-05-21 22:41:44 -0400

    update simscape model

--- Day_02_PID_Controller/Day_02.m
+++ Day_02_PID_Controller/Day_02.m
@@ -2,9 +2,9 @@
 %% PID Controller with Fixed Base
 %% Setup
 qb_d = 0;
-q1_d = pi;
-q2_d = pi;
-q3_d = pi;
+q1_d = 0;
+q2_d = 0;
+q3_d = 0;
 
 %% Displaying the Result
 figure(1)
@@ -17,8 +17,8 @@ ylabel('Angular Displacement [rads]')
 %% PID Controller with Moving Base
 %% Displaying the Result
 figure(1)
-plot(t, qb*10, t, q1, t, q2, t, q3)
+plot(t, qb, t, q1, t, q2, t, q3)
 title('Time vs. Joint Parameters')
-legend('Base * 10', 'Link 1', 'Link 2', 'Link 3', 'Location', 'best')
+legend('Base', 'Link 1', 'Link 2', 'Link 3', 'Location', 'best')
 xlabel('Time [s]')
 ylabel('Angular Displacement [rads]')
